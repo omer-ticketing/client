11ef7c3cd5ddee8e1d6319193a5f09bd8ab23d63
omer <omerme13@gmail.com>
2024-06-30 21:37:06 +0300

    feat(signup): on submit req

--- pages/auth/signup.jsx
+++ pages/auth/signup.jsx
@@ -1,14 +1,27 @@
+import { useState } from "react";
+import axios from 'axios';
+
 const Signup = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+	const onSubmit = async (e) => {
+		e.preventDefault();
+		await axios.post('/api/users/signup', {
+			email, password
+		})
+	};
+
     return (
-        <form>
+        <form onSubmit={onSubmit}>
             <h1>Sign Up</h1>
             <div className="form-group">
                 <label>Email Address</label>
-                <input type="text" className="form-control" />
+                <input type="text" className="form-control" onChange={(e) => setEmail(e.target.value)} />
             </div>
             <div className="form-group">
                 <label>Password</label>
-                <input type="text" className="form-control" />
+                <input type="password" className="form-control" onChange={(e) => setPassword(e.target.value)} />
             </div>
             <button className="btn btn-primary">Sign Up</button>
         </form>
