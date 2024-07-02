9d6c082996244a628ca31113d97db6012ba3526a
omer <omerme13@gmail.com>
2024-07-02 21:12:48 +0300

    feat: get curUser to the home comp

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,3 +1,24 @@
-export default () => {
-	return <h1>Home</h1>
-}
+import axios from "axios";
+import { authServiceUrl } from "../utils/constants";
+
+const Home = ({ currentUser }) => {
+    return <h1>You're Signed {currentUser ? 'in' : 'out'}</h1>;
+};
+
+export const getServerSideProps = async ({ req }) => {
+    try {
+        const headers = req.headers.cookie ? { Cookie: req.headers.cookie } : {};
+        const res = await axios.get(`${authServiceUrl}/api/users/current-user`, { headers });
+
+        return {
+            props: { currentUser: res.data.data.user },
+        };
+    } catch (error) {
+        console.error("Error fetching current user:", error.response.data?.errors);
+        return {
+            props: { currentUser: null },
+        };
+    }
+};
+
+export default Home;
