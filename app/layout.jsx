dacfed5a814f63f60c5e3136605cc444fb7ed058
omer <omerme13@gmail.com>
2024-07-06 16:12:13 +0300

    feat: add header. refactor the layout and page file

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,10 +1,31 @@
 import "bootstrap/dist/css/bootstrap.css";
+import Header from "../components/Header";
+import { cookies } from "next/headers";
+import axios from "axios";
+import { authServiceUrl } from "../utils/constants";
 
-export default ({ children }) => {
+const getCurrentUser = async () =>  {
+    const cookieStore = cookies();
+    let cookieHeader = cookieStore.toString();
+	cookieHeader = decodeURIComponent(cookieHeader);
+
+    try {
+        const headers = cookieHeader ? { Cookie: cookieHeader } : {};
+
+        const res = await axios.get(`${authServiceUrl}/api/users/current-user`, { headers });
+        return res.data.data.user;
+    } catch (error) {
+        console.error("Error fetching current user:", error.response?.data?.errors || error.message);
+        return null;
+    }
+}
+
+export default async ({ children }) => {
+	const currentUser = await getCurrentUser();
     return (
         <html lang="en">
             <body>
-                <h3>This is a test header</h3>
+                <Header currentUser={currentUser} />
                 {children}
             </body>
         </html>
---
+++ components/Header.jsx
@@ -0,0 +1,24 @@
+import Link from 'next/link';
+
+export default ({ currentUser }) => {
+	const links = [
+		!currentUser && {label: 'Sign Up', href: '/auth/signup'},
+		!currentUser && {label: 'Sign In', href: '/auth/signin'},
+		currentUser && {label: 'Sign Out', href: '/auth/signout'},
+	]
+	.filter(link => link)
+	.map(({label, href}) => (
+		<li key={label} className='nav-item'>
+			<Link href={href} className='nav-link'>{label}</Link>
+		</li>
+	))
+
+	return <nav className="navbar navbar-light bg-light p-2">
+		<Link href="/" className='navbar-brand'>GitTix</Link>
+		<div className="d-flex justify-content-end">
+			<ul className='nav d-flex align-items-center'>
+				{links}
+			</ul>
+		</div>
+	</nav>
+}
